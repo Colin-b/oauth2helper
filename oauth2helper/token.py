c28f2d6b7d2a0dafa1a160d97071c1c516746809
JS5391 <colin.bounouar@engie.com>
2018-09-04 17:27:53 +0200

    Do not allow to skip JSON token validation

--- oauth2helper/token.py
+++ oauth2helper/token.py
@@ -37,8 +37,7 @@ def validate(jwt_token: str, **options) -> (dict, dict):
     :raises InvalidKeyError
     """
     json_header, json_body = decode(jwt_token)
-    if options.get('verify_signature', True):
-        _validate_json_token(jwt_token, json_header, json_body, options)
+    _validate_json_token(jwt_token, json_header, json_body, options)
     return json_header, json_body
 
 
