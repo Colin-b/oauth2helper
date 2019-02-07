9cab8a88c15e3eccb22db6a89171d42ea0c75611
JS5391 <colin.bounouar@engie.com>
2019-02-07 20:20:18 +0100

    Add pre-commit hook for black

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-# Provide information on OAuth2 #
+<h2 align="center">OAuth2 helper</h2>
 
-This python module provides:
+<p align="center">
+<a href="https://github.com/ambv/black"><img alt="Code style: black" src="https://img.shields.io/badge/code%20style-black-000000.svg"></a>
+<a href='https://pse.tools.digital.engie.com/drm-all.gem/job/team/view/Python%20modules/job/oauth2helper/job/master/'><img src='https://pse.tools.digital.engie.com/drm-all.gem/buildStatus/icon?job=team/oauth2helper/master'></a>
+</p>
+
+Provides:
 
  * Validation of OAuth2 token
  * Extraction of data from validated (or decoded) token.
@@ -28,3 +33,16 @@ my_token = headers.get('Bearer')
 json_header, json_body = validate(my_token)
 username = user_name(json_body)
 ```
+
+Contributing
+------------
+
+Everyone is free to contribute on this project.
+
+Before creating an issue please make sure that it was not already reported.
+
+Project follow "Black" code formatting: https://black.readthedocs.io/en/stable/
+
+To integrate it within Pycharm: https://black.readthedocs.io/en/stable/editor_integration.html#pycharm
+
+To add the pre-commit hook, after the installation run: **pre-commit install**
--- oauth2helper/token.py
+++ oauth2helper/token.py
@@ -8,14 +8,14 @@ import logging
 from jwt.exceptions import InvalidTokenError
 
 _default_options = {
-    'verify_signature': True,
-    'verify_exp': True,
-    'verify_nbf': True,
-    'verify_iat': True,
-    'verify_aud': False,
-    'require_exp': True,
-    'require_iat': True,
-    'require_nbf': True
+    "verify_signature": True,
+    "verify_exp": True,
+    "verify_nbf": True,
+    "verify_iat": True,
+    "verify_aud": False,
+    "require_exp": True,
+    "require_iat": True,
+    "require_nbf": True,
 }
 
 
@@ -47,59 +47,71 @@ def decode(jwt_token: str) -> (dict, dict):
     :raises InvalidTokenError
     """
     if not jwt_token:
-        raise InvalidTokenError('JWT Token is mandatory.')
+        raise InvalidTokenError("JWT Token is mandatory.")
 
-    if jwt_token.count('.') < 2:
-        raise InvalidTokenError('Invalid JWT Token (header, body and signature must be separated by dots).')
+    if jwt_token.count(".") < 2:
+        raise InvalidTokenError(
+            "Invalid JWT Token (header, body and signature must be separated by dots)."
+        )
 
-    (jwt_header, jwt_body, jwt_signature) = jwt_token.split('.', maxsplit=2)
+    (jwt_header, jwt_body, jwt_signature) = jwt_token.split(".", maxsplit=2)
 
     return _to_json(jwt_header), _to_json(jwt_body)
 
 
-def _validate_json_token(jwt_token: str, json_header: dict, json_body: dict, options: dict):
+def _validate_json_token(
+    jwt_token: str, json_header: dict, json_body: dict, options: dict
+):
     public_key = _get_public_key(json_body, json_header)
-    logging.debug(f'Public key: {public_key}')
+    logging.debug(f"Public key: {public_key}")
     jwt.decode(jwt_token, public_key, options={**_default_options, **options})
 
 
 def _get_public_key(json_body: dict, json_header: dict):
-    key_identifier = json_header.get('kid')
+    key_identifier = json_header.get("kid")
     if not key_identifier:
-        raise InvalidTokenError('Key identifier (kid) cannot be found in JSON header.')
+        raise InvalidTokenError("Key identifier (kid) cannot be found in JSON header.")
 
     # TODO cache this
     x5c = _request_x5c(json_body, key_identifier)
 
-    certificate_text = b"-----BEGIN CERTIFICATE-----\n" + x5c.encode('utf-8') + b"\n-----END CERTIFICATE-----"
+    certificate_text = (
+        b"-----BEGIN CERTIFICATE-----\n"
+        + x5c.encode("utf-8")
+        + b"\n-----END CERTIFICATE-----"
+    )
     certificate = load_pem_x509_certificate(certificate_text, default_backend())
     return certificate.public_key()
 
 
 def _request_x5c(json_body: dict, key_identifier: str) -> str:
-    identity_supplier_service = json_body.get('iss')
+    identity_supplier_service = json_body.get("iss")
     if identity_supplier_service is None:
-        raise InvalidTokenError('Identity supplier service (iss) cannot be found in JSON body.')
+        raise InvalidTokenError(
+            "Identity supplier service (iss) cannot be found in JSON body."
+        )
 
-    tenant_identifier = json_body.get('tid')
+    tenant_identifier = json_body.get("tid")
     if tenant_identifier is None:
-        raise InvalidTokenError('Tenant identifier (tid) cannot be found in JSON body.')
+        raise InvalidTokenError("Tenant identifier (tid) cannot be found in JSON body.")
 
     identity_provider_url = identity_supplier_service.split(tenant_identifier)[0]
 
-    keys_response = requests.get(f'{identity_provider_url}common/discovery/keys')
-    keys = keys_response.json().get('keys', [])
+    keys_response = requests.get(f"{identity_provider_url}common/discovery/keys")
+    keys = keys_response.json().get("keys", [])
 
     for key in keys:
-        if key['kid'] == key_identifier:
-            return key['x5c'][0]
+        if key["kid"] == key_identifier:
+            return key["x5c"][0]
 
-    raise InvalidTokenError(f'{key_identifier} is not a valid key identifier. Valid ones are {[key["kid"] for key in keys]}.')
+    raise InvalidTokenError(
+        f'{key_identifier} is not a valid key identifier. Valid ones are {[key["kid"] for key in keys]}.'
+    )
 
 
 def _to_json(base_64_json: str) -> dict:
     decoded_json = decode_base64(base_64_json)
-    return json.loads(decoded_json.decode('unicode_escape'))
+    return json.loads(decoded_json.decode("unicode_escape"))
 
 
 def decode_base64(base64_encoded_string: str) -> bytes:
@@ -111,5 +123,5 @@ def decode_base64(base64_encoded_string: str) -> bytes:
     """
     missing_padding = len(base64_encoded_string) % 4
     if missing_padding != 0:  # Pad with extra = characters at the end
-        base64_encoded_string += '=' * (4 - missing_padding)
-    return base64.b64decode(base64_encoded_string, altchars='_+')
+        base64_encoded_string += "=" * (4 - missing_padding)
+    return base64.b64decode(base64_encoded_string, altchars="_+")
